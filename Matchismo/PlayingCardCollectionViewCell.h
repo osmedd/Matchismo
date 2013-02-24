a664fd99a35ba65288871f59f35694370e00aae4
Owen Medd <osm@interguide.com>
2013-02-24 16:06:10 -0500

    Assignment 3: Use UICollectionView for PlayingCard game

---
+++ Matchismo/PlayingCardCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardCollectionViewCell.h
+//  Matchismo
+//
+//  Created by Owen Medd on 2/24/13.
+//  Copyright (c) 2013 InterGuide Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayingCardCollectionViewCell : UICollectionViewCell
+
+@end
